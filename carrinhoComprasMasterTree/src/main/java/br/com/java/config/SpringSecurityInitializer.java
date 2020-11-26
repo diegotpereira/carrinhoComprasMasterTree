250b28e5d00a0d2b3e05ee766904725e3b319d39
Diego <diegoteixeirapereira@hotmail.com>
2020-11-26 10:03:17 -0300

    criação do arquivo classe springsecurityinitializer.java

---
+++ carrinhoComprasMasterTree/src/main/java/br/com/java/config/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package br.com.java.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
