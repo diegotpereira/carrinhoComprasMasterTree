78188cc412700ad0d7d41e13342b1fe5844ee085
Diego <diegoteixeirapereira@hotmail.com>
2020-11-26 10:02:38 -0300

    alteração no arquivo classe appwebconfiguration.java

--- carrinhoComprasMasterTree/src/main/java/br/com/java/config/AppWebConfiguration.java
+++ carrinhoComprasMasterTree/src/main/java/br/com/java/config/AppWebConfiguration.java
@@ -1,17 +1,32 @@
 package br.com.java.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import br.com.java.dao.ContaDAO;
+import br.com.java.dao.ContaDAOImpl;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "br.com.java")
 public class AppWebConfiguration {
+	
+	@Autowired
+	private Environment env;
 	
 	@Bean(name="AppWeb")
 	public ViewResolver viewResolver() {
@@ -22,5 +37,45 @@ public class AppWebConfiguration {
 
 		return viewResolver;
 	}
+	
+	@Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+        Properties properties = new Properties();
+ 
+        // See: ds-hibernate-cfg.properties
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("current_session_context_class", env.getProperty("current_session_context_class"));
+         
+ 
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+         
+        // Package contain entity classes
+        factoryBean.setPackagesToScan(new String[] { "br.com.java.entity" });
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.afterPropertiesSet();
+        //
+        SessionFactory sf = factoryBean.getObject();
+        System.out.println("## getSessionFactory: " + sf);
+        return sf;
+    }
+	@Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+ 
+        return transactionManager;
+    }
+	@Bean(name = "dao")
+    public ContaDAO getApplicantDAO() {
+        return new ContaDAOImpl();
+    }
+	
+	@Bean(name = "dao")
+    public ContaDAO getContaDAO()  {
+        return new ContaDAOImpl();
+    }
 
 }
